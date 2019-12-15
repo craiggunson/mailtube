8f3225fd8f81133a14fdf373657a4885b18b7a83
Craig Gunson <Craig.Gunson@sportsbet.com.au>
2019-12-15 19:05:40 +1100

    working mailparser

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 console.log('Loading function');
 
 const aws = require('aws-sdk');
-const mailParser = require('mailparser').mailParser;
+const simpleParser = require('mailparser').simpleParser;
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
-
 
 exports.handler = async (event, context) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
@@ -14,9 +13,17 @@ exports.handler = async (event, context) => {
         Key: key,
     };
     try {
-        const { ContentType } = await s3.getObject(params).promise();
-        console.log('CONTENT TYPE:', ContentType);
-        return ContentType;
+        const data = await s3.getObject(params).promise();
+        //console.log('raw email:' + data.Body);
+        const email = await simpleParser(data.Body);
+        console.log('date:', email.date);
+        console.log('subject:', email.subject);
+        console.log('body:', email.text);
+        console.log('from:', email.from.text);
+        console.log('attachments:', email.attachments);
+
+        return { status: 'success' };
+
     } catch (err) {
         console.log(err);
         const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
